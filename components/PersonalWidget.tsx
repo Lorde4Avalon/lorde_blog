3448ec6e073260bf743a666d21ba6eaf733d8196
lorde <rioaformyking@gmail.com>
2022-09-20 16:08:11 +0800

    feat: add github link and svg

--- components/PersonalWidget.tsx
+++ components/PersonalWidget.tsx
@@ -10,7 +10,6 @@ const PersonalWidget = () => {
 
   useEffect(() => {
     getAuthor().then((result) => {
-      console.log(result.author.data.attributes);
       setAuthor(result.author.data.attributes);
     })
   }, [])
@@ -24,14 +23,17 @@ const PersonalWidget = () => {
       <p className='text-center mt-1 text-lg'>
         {author.name}
       </p>
-      <div className="flex justify-center items-center p-2 font-sans text-lg font-medium">
-        <div className="inline-block mx-6 cursor-pointer">
+      <div className="block lg:flex text-center items-center justify-center p-2 font-sans text-lg font-medium">
+        <a href='https://github.com/Lorde4Avalon' target="_blank" className="inline-block mx-3">
+          <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" className="bi bi-github inline mr-1 mb-1" viewBox="0 0 16 16">
+            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+          </svg>
           GitHub
-        </div>
-        <div className="inline-block mx-6 cursor-pointer">Subscript</div>
+        </a>
+        <div className="inline-block mx-3 cursor-pointer">Subscript</div>
       </div>
       <p className='text-center text-lg font-normal pt-6'>
-        Thinking for rationality, Working for liberty.
+        {author.bio}
       </p>
     </div>
   )
