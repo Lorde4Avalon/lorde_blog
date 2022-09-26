8ba4e32706cd04d6bcee3603753092d9bc5b7e3b
lorde <rioaformyking@gmail.com>
2022-09-26 15:12:51 +0800

    feat: add heart icon for footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { AiTwotoneHeart } from 'react-icons/ai'
 
 const Footer = () => {
     return (
         <footer className='container mx-auto px-10 '>
             <div className="flex text-center justify-center text-gray-300 px-6 py-1">
-                @2022 code & design with  by
+                @2022 code & design with <AiTwotoneHeart className='mt-1 mx-1'/> by
                 <a className='px-2 underline' href="https://github.com/Lorde4Avalon" target="_blank">Lorde</a>
             </div>
             <div className="flex text-center justify-center text-gray-300">
