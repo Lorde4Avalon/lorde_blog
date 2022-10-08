4c3cb7989ac9e0a6167ddc9e9b78812480b92b8a
lorde <rioaformyking@gmail.com>
2022-10-08 20:11:55 +0800

    feat: change footer to flex-wrap

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,9 @@ import { AiTwotoneHeart } from 'react-icons/ai'
 const Footer = () => {
     return (
         <footer className='container mx-auto px-10 '>
-            <div className="flex text-center justify-center text-gray-300 px-6 py-1">
+            <div className="flex flex-wrap justify-center text-gray-300 px-6 py-1">
                 @2022 code & design with <AiTwotoneHeart className='mt-1 mx-1'/> by
-                <a className='px-2 underline' href="https://github.com/Lorde4Avalon" target="_blank">Lorde</a>
+                <a className='px-2 underline ' href="https://github.com/Lorde4Avalon" target="_blank">Lorde</a>
             </div>
             <div className="flex text-center justify-center text-gray-300">
                 Made by NextJs TailwindCSS
