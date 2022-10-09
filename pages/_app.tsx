c5a27801dd453912d5764d8e55a77bb4845d8e45
lorde <rioaformyking@gmail.com>
2022-10-09 13:09:10 +0800

    feat: move head to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,15 @@ import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 
 import { Layout } from '../components'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Avalon</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage, GetStaticProps, InferGetStaticPropsType } from 'next'
 import type { post } from '../types'
-import Head from 'next/head'
 
 import { PostCard, PersonalWidget, PostWidget, RelateWidget, Categories, Pagination } from '../components'
 import { getPosts } from '../services'
@@ -26,10 +25,6 @@ const Home: NextPage = ({ posts }: InferGetStaticPropsType<GetStaticProps>) => {
 
   return (
     <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>Avalon</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
