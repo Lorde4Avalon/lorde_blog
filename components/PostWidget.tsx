680fc83959f14757c1a8ff2f954de3c9f2138a21
lorde <rioaformyking@gmail.com>
2022-10-11 21:31:01 +0800

    feat: fix RecentPost Date

--- components/PostWidget.tsx
+++ components/PostWidget.tsx
@@ -11,6 +11,9 @@ const host = process.env.CMS_ENDPOINT
 
 const PostWidget = ({ posts }: Props) => {
 
+  console.log(posts);
+  
+
   return (
     <div className="bg-white rounded-lg shadow-lg p-8 mb-8">
       <h2 className='font-semibold text-lg border-b mb-3'>Recent Posts</h2>
@@ -18,7 +21,7 @@ const PostWidget = ({ posts }: Props) => {
         return <a href={`/post/${post.attributes.slug}`} key={post.id}>
           <div className="bg-zinc-700 my-3 rounded-2xl cursor-pointer ease-out hover:bg-blue-600 transition duration-700 hover:-translate-y-1 flex flex-col justify-center">
             <h3 className='text-center text-white'>{post.attributes.title}</h3>
-            <span className='text-center text-base text-white'>{moment(post.createdAt).format('MMM DD, YYYY')}</span>
+            <span className='text-center text-base text-white'>{moment(post.attributes.createdAt).format('MMM DD, YYYY')}</span>
           </div>
         </a>
       })}
