b1cb4f39c04b63fb1779fdd93390a7bd1a181ec9
lorde <rioaformyking@gmail.com>
2022-10-12 12:09:49 +0800

    feat: change recent to  next/Link

--- components/PostWidget.tsx
+++ components/PostWidget.tsx
@@ -1,4 +1,5 @@
 import moment from 'moment'
+import Link from 'next/link'
 import React from 'react'
 
 interface Props {
@@ -9,21 +10,18 @@ interface Props {
 
 const host = process.env.CMS_ENDPOINT
 
-const PostWidget = ({ posts }: Props) => {
-
-  console.log(posts);
-  
+const PostWidget = ({ posts }: Props) => {  
 
   return (
     <div className="bg-white rounded-lg shadow-lg p-8 mb-8">
       <h2 className='font-semibold text-lg border-b mb-3'>Recent Posts</h2>
       {posts.map((post: any) => {
-        return <a href={`/post/${post.attributes.slug}`} key={post.id}>
+        return <Link href={`/post/${post.attributes.slug}`} key={post.id}>
           <div className="bg-zinc-700 my-3 rounded-2xl cursor-pointer ease-out hover:bg-blue-600 transition duration-700 hover:-translate-y-1 flex flex-col justify-center">
             <h3 className='text-center text-white'>{post.attributes.title}</h3>
             <span className='text-center text-base text-white'>{moment(post.attributes.createdAt).format('MMM DD, YYYY')}</span>
           </div>
-        </a>
+        </Link>
       })}
     </div>
   )
